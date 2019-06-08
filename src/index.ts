1217f61a345b72a2ac1352b00041479c5579b0e6
shenwl <shenweilun96@gmail.com>
2019-06-08 14:19:07 +0800

    transformRequestData

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { RequestConfig } from './types'
 import { buildURL } from './tools/url'
+import { transformRequest } from './tools/data'
 import xhr from './xhr'
 
 function request(config: RequestConfig | string) {
-
   if (typeof config === 'string') {
     xhr({ url: config })
   } else {
@@ -15,12 +15,18 @@ function request(config: RequestConfig | string) {
 function processConfig(config: RequestConfig): RequestConfig {
   const requestConfig = { ...config }
   requestConfig.url = transformURL(requestConfig)
+  requestConfig.data = transformRequestData(requestConfig)
   return requestConfig
 }
 
 function transformURL(config: RequestConfig): string {
   const { url, params } = config
   return buildURL(url, params)
+}
+
+function transformRequestData(config: RequestConfig): any {
+  const { data } = config
+  return transformRequest(data)
 }
 
 export default request
---
+++ src/tools/data.ts
@@ -0,0 +1,8 @@
+import { isPlainObject } from './utils'
+
+export const transformRequest = (data: any): any => {
+  if (isPlainObject(data)) {
+    return JSON.stringify(data)
+  }
+  return data
+}
