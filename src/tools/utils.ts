2b67aceac37b37d0d9233bb97a52ad8c1d95660f
shenwl <shenweilun96@gmail.com>
2019-06-08 14:15:09 +0800

    普通对象判断

--- src/tools/utils.ts
+++ src/tools/utils.ts
@@ -1,9 +1,13 @@
 const toString = Object.prototype.toString
 
-export const isDate = (val: any): boolean => {
+export const isDate = (val: any): val is Date => {
   return toString.call(val) === "[object Date]"
 }
 
-export const isObject = (val: any): boolean => {
+export const isObject = (val: any): val is Object => {
   return val !== null && typeof val === 'object'
+}
+
+export const isPlainObject = (val: any): val is Object => {
+  return toString.call(val) === "[object Object]"
 }
