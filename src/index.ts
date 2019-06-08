d159ea2c87cb459eaa4b8d064b7282b86915328e
shenwl <shenweilun96@gmail.com>
2019-06-08 12:51:38 +0800

    处理传入的config

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,26 @@
 import { RequestConfig } from './types'
+import { buildURL } from './tools/url'
 import xhr from './xhr'
 
 function request(config: RequestConfig | string) {
+
   if (typeof config === 'string') {
     xhr({ url: config })
   } else {
-    xhr(config)
+    const requestConfig = processConfig(config)
+    xhr(requestConfig)
   }
+}
+
+function processConfig(config: RequestConfig): RequestConfig {
+  const requestConfig = { ...config }
+  requestConfig.url = transformURL(requestConfig)
+  return requestConfig
+}
+
+function transformURL(config: RequestConfig): string {
+  const { url, params } = config
+  return buildURL(url, params)
 }
 
 export default request
