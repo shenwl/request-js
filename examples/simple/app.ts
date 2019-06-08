b599a89076721ea3a0cab952923b7f40e223ae5a
shenwl <shenweilun96@gmail.com>
2019-06-08 13:35:53 +0800

    simple demo

--- examples/simple/app.ts
+++ examples/simple/app.ts
@@ -4,24 +4,10 @@ request('/simple/get')
 
 request({
   url: '/simple/get',
-  params: {
-    foo: ['bar', 'baz']  
-  }
+  method: 'GET',
 })
 
 request({
   url: '/simple/get',
-  params: {
-    foo: {
-      bar: 'bar',
-      baz: 'baz',
-    }
-  }
-})
-
-request({
-  url: '/simple/get',
-  params: {
-    date: new Date()
-  }
+  method: 'get',
 })
