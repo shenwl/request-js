32ca5dd1667732758a833367cdffa6c27ddf3006
shenwl <shenweilun96@gmail.com>
2019-06-13 22:25:49 +0800

    测试返回是否promise

--- examples/simple/app.ts
+++ examples/simple/app.ts
@@ -5,9 +5,13 @@ request('/simple/get')
 request({
   url: '/simple/get',
   method: 'GET',
-})
+}).then(res => {
+  console.log(res)
+}).catch(() => {})
 
 request({
   url: '/simple/get',
   method: 'get',
-})
+}).then(res => {
+  console.log(res)
+}).catch(() => {})
