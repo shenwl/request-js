4fac81d52a331af67a14d34cca88b69409a7f474
shenwl <shenweilun96@gmail.com>
2019-06-13 22:23:27 +0800

    返回promise

--- src/xhr.ts
+++ src/xhr.ts
@@ -1,16 +1,38 @@
-import { RequestConfig } from './types'
+import { RequestPromise, RequestConfig, Response } from './types'
 
-export default (config: RequestConfig): void => {
-  const { data = null, url, method = 'get', headers = {} } = config
+export default (config: RequestConfig): RequestPromise => {
+  return new Promise((resolve, reject) => {
+    const { data = null, url, method = 'get', headers = {}, responseType } = config
 
-  const xhr = new XMLHttpRequest()
+    const xhr = new XMLHttpRequest()
 
-  xhr.open(method.toUpperCase(), url, true)
-  Object.entries(headers).forEach(([name, value]) => {
-    if(data === null && name.toLowerCase() === 'content-type') {
-      delete headers[name]
+    if (responseType) {
+      xhr.responseType = responseType
     }
-    typeof value === 'string' && xhr.setRequestHeader(name, value)
+
+    xhr.open(method.toUpperCase(), url, true)
+    Object.entries(headers).forEach(([name, value]) => {
+      if (data === null && name.toLowerCase() === 'content-type') {
+        delete headers[name]
+      }
+      typeof value === 'string' && xhr.setRequestHeader(name, value)
+    })
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState !== 4) {
+        return
+      }
+      const responseHeaders = xhr.getAllResponseHeaders()
+      const responseData = responseType !== 'text' ? xhr.response : xhr.responseText
+      const response: Response = {
+        data: responseData,
+        status: xhr.status,
+        statusText: xhr.statusText,
+        headers: responseHeaders,
+        config,
+        request: xhr,
+      }
+      resolve(response)
+    }
+    xhr.send(data)
   })
-  xhr.send(data)
 }
