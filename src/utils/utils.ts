c83205af477b860c764d694bd3494ef34bccef95
shenwl <shenweilun96@gmail.com>
2019-07-06 19:14:58 +0800

    实现extend

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -11,3 +11,10 @@ export const isObject = (val: any): val is Object => {
 export const isPlainObject = (val: any): val is Object => {
   return toString.call(val) === "[object Object]"
 }
+
+export function extend<T, F>(to: T, from: F): T & F {
+ for(const key in from) {
+   (to as T & F)[key] = from[key] as any
+ }
+ return to as T & F;
+}
